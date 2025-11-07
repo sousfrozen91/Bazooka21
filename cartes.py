b2337cd650b3bee3b8dddafaa81a24b6e8ca433f
sousfrozen91 <hugonnotmathias@gmail.com>
2025-11-07 09:28:59 +0100

    Refactor Cartes class to use 'name' instead of 'val'

--- cartes.py
+++ cartes.py
@@ -1,6 +1,33 @@
 Class Cartes :
-    def __init__(self,val,points,couleur):
-        self.val = val
+    def __init__(self,name,points,couleur):
+        self.name = name
         self.points = points
         self.couleur = couleur
 
+'''liste cartes, un seul jeu ici'''
+'1c' = Cartes('As',1,'coeur')
+'2c' = Cartes('2',2,'coeur')
+'3c' = Cartes('3',3,'coeur')
+'4c' = Cartes('4',4,'coeur')
+'5c' = Cartes('5',5,'coeur')
+'6c' = Cartes('6',6,'coeur')
+'7c' = Cartes('7',7,'coeur')
+'8c' = Cartes('8',8,'coeur')
+'9c' = Cartes('9',9,'coeur')
+'10c' = Cartes('10',10,'coeur')
+'Vc' = Cartes('Valet',10,'coeur')
+'Dc' = Cartes('Dame',10,'coeur')
+'Rc' = Cartes('Roi',10,'coeur')
+'1p' = Cartes('1',1,'pique')
+'2p' = Cartes('2',2,'pique')
+'3p' = Cartes('3',3,'pique')
+'4p' = Cartes('4',4,'pique')
+'5p' = Cartes('5',5,'pique')
+'6p' = Cartes('6',6,'pique')
+'7p' = Cartes('7',7,'pique')
+'8p' = Cartes('8',8,'pique')
+'9p' = Cartes('9',9,'pique')
+'11p' = Cartes('10',10,'pique')
+'Vp' = Cartes('Valet',10,'pique')
+'Dp' = Cartes('Dame',10,'pique')
+'Rp' = Cartes('Roi',10,'pique')
