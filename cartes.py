2ddea5c385b9b41623edda03ecc4973d591cfaf0
marty69 <marceaugoubet1@gmail.com>
2025-11-21 09:44:48 +0100

    Update cartes.py

--- cartes.py
+++ cartes.py
@@ -1,33 +1,16 @@
-
-
-jeu_blackjack = {
-    # PIQUE
-    "As de Pique": 11, "Roi de Pique": 10, "Dame de Pique": 10, "Valet de Pique": 10,
-    "10 de Pique": 10, "9 de Pique": 9, "8 de Pique": 8, "7 de Pique": 7,
-    "6 de Pique": 6, "5 de Pique": 5, "4 de Pique": 4, "3 de Pique": 3, "2 de Pique": 2,
-
-    # COEUR
-    "As de Coeur": 11, "Roi de Coeur": 10, "Dame de Coeur": 10, "Valet de Coeur": 10,
-    "10 de Coeur": 10, "9 de Coeur": 9, "8 de Coeur": 8, "7 de Coeur": 7,
-    "6 de Coeur": 6, "5 de Coeur": 5, "4 de Coeur": 4, "3 de Coeur": 3, "2 de Coeur": 2,
-
-    # CARREAU
-    "As de Carreau": 11, "Roi de Carreau": 10, "Dame de Carreau": 10, "Valet de Carreau": 10,
-    "10 de Carreau": 10, "9 de Carreau": 9, "8 de Carreau": 8, "7 de Carreau": 7,
-    "6 de Carreau": 6, "5 de Carreau": 5, "4 de Carreau": 4, "3 de Carreau": 3, "2 de Carreau": 2,
-
-    # TRÈFLE
-    "As de Trèfle": 11, "Roi de Trèfle": 10, "Dame de Trèfle": 10, "Valet de Trèfle": 10,
-    "10 de Trèfle": 10, "9 de Trèfle": 9, "8 de Trèfle": 8, "7 de Trèfle": 7,
-    "6 de Trèfle": 6, "5 de Trèfle": 5, "4 de Trèfle": 4, "3 de Trèfle": 3, "2 de Trèfle": 2
-}
-
 class Carte :
-    def __init__(self, valeur, couleur):
-        self.valeur = valeur
-        self.couleur = couleur
+    def __init__(self):
+        self.valeur = ["As", "Roi", "Dame", "Valet", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
+        self.couleur = ["Pique", "Coeur", "Carreau", "Trèfle"]
+        self.valeur_jeu = [11, 10, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2]
 
+    for i in self.valeur:
+        for j in self.couleur:
+            dico =
+            
+        
 
     
         
+
 
