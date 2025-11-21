91da8de5bac4eeea180f2eddd57a01be53dd9864
sousfrozen91 <hugonnotmathias@gmail.com>
2025-11-21 08:40:33 +0100

    Refactor Cartes class and define Carte class

--- cartes.py
+++ cartes.py
@@ -1,8 +1,7 @@
 
 
-
-
-
-class Cartes :    
-    def __init__(self,name,val,couleur,etat):
-        self.name
+coul = ["carreau","coeur","pique","trefle"]
+val = [1,2,3,4,5,6,7,8,9,10,10,10,10]
+class Carte :
+    def tirage(self) :
+        
